a98db5ab6ea8d5d4d3443f50e1a721c4a97697bb
ostafon1 <154944433+ostafon1@users.noreply.github.com>
2024-04-22 19:57:17 -0500

    added outputs

---
+++ vpc/output.tf
@@ -0,0 +1,9 @@
+output vpc_id {
+    value = module.vpc.vpc_id
+}
+output private_subnets {
+    value = module.vpc.private_subnets
+}
+output public_subnets {
+    value = module.vpc.private_subnets
+}
